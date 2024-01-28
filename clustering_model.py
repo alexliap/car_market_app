b93df61154538e957b1f802c1006c7eb4fab673a
alexliap <alexandrosliapates@gmail.com>
2024-01-28 19:18:48 +0200

    Update files

---
+++ clustering_model.py
@@ -0,0 +1,58 @@
+import polars as pl
+from sklearn.cluster import HDBSCAN
+
+features = pl.read_csv("CleanData/features.csv")
+listings = pl.read_csv("CleanData/listings.csv")
+mapping = pl.read_csv("CleanData/id_mapping.csv")
+
+features = features.drop(
+    [
+        "id_make",
+        "niceName_make",
+        "niceName_model",
+        "id_engine",
+        "id_transmission",
+        "name_transmission",
+        "numberOfSpeeds_transmission",
+        "vehicleType_categories",
+        "vehicleStyle_categories",
+        "automaticType_transmission",
+        "errorType",
+        "message",
+    ]
+)
+
+new_data = (
+    listings.select(["vin", "modelId", "priceUnformatted"])
+    .join(features, on="vin", how="inner")
+    .drop("vin")
+)
+
+num_cols = new_data.select(pl.col([pl.Float64, pl.Int64])).drop("modelId").columns
+str_cols = new_data.select(pl.col(pl.String)).columns
+cls_data = new_data
+
+for col in num_cols:
+    mean = cls_data[col].mean()
+    std = cls_data[col].std()
+
+    cls_data = cls_data.with_columns(((cls_data[col] - mean) / std).alias(col))
+
+cls_data = cls_data.to_dummies(str_cols)
+
+hdb = HDBSCAN(
+    min_cluster_size=40,
+    metric="euclidean",
+    algorithm="auto",
+    n_jobs=4,
+    max_cluster_size=100,
+    cluster_selection_method="leaf",
+    leaf_size=50,
+)
+hdb.fit(cls_data)
+
+cls_data = cls_data.with_columns(segment=hdb.labels_)
+cls_data = cls_data.join(mapping, on="modelId", how="inner")
+
+cls_data.write_csv("ClusteringData/data.csv")
+cls_data.write_csv("app/data/ClusteringData/data.csv")
--- mapping.py
+++ mapping.py
@@ -1,9 +1,10 @@
 import polars as pl
 
-features = pl.read_csv("CleanData/features.csv")
 listings = pl.read_csv("CleanData/listings.csv")
 
 mapping = listings.select(["make", "model", "modelId"])
 mapping = mapping.with_columns(make_model=pl.col("make") + " " + pl.col("model"))
 mapping = mapping.unique()
+
 mapping.write_csv("CleanData/id_mapping.csv")
+mapping.write_csv("app/data/Mappings/id_mapping.csv")
